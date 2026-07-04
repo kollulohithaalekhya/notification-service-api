cdc86b7143bfd1bb99093df2bbabfc9f4fca7aac
kollulohithaalekhya <147791802+kollulohithaalekhya@users.noreply.github.com>
2026-07-04 14:40:34 +0530

    test: configure Jest testing framework

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/server.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  // Increase timeout for mongodb-memory-server startup
+  testTimeout: 30000,
+};
